d72e4b1e6bf3590b9002e7efb7c4bc41ecfa9559
Guilherme Rosa <guilherme@live.com>
2020-05-11 17:27:54 -0300

    upload de arquivos funcionando

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -2,71 +2,84 @@
 
 namespace App\Http\Controllers\Api;
 
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 use DB;
+use Hash;
 
 class PostController extends Controller
 {
     //
-	public function upload_img(Request $request){
-		if($request->hasFile('photo')):
+	public function publicar(Request $request){
+		$name = Hash::make(date('Y-m-d H:i:s'));
 
-			$target_dir = "assets/img/posts/users/";
-			$target_file = $target_dir . basename($_FILES["photo"]["name"]);
+		if($request->hasFile('photo')):
+			$target_dir = "assets/img/posts/users/" . auth()->user()->id . "/";
+			$target_file = $target_dir . basename($name) . '.jpg';
 			$uploadOk = 1;
 			$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
-		// Check if image file is a actual image or fake image
-		if(isset($_POST["submit"])):
-			$check = getimagesize($_FILES["photo"]["tmp_name"]);
-			if($check !== false):
-				echo "File is an image - " . $check["mime"] . ".";
-				$uploadOk = 1;
-			else:
-				echo "File is not an image.";
+			// Check if image file is a actual image or fake image
+			if(isset($_POST["submit"])):
+				$check = getimagesize($_FILES["photo"]["tmp_name"]);
+				if($check !== false):
+					echo "File is an image - " . $check["mime"] . ".";
+					$uploadOk = 1;
+				else:
+					echo "File is not an image.";
+					$uploadOk = 0;
+				endif;
+			endif;
+
+			// Check if file already exists
+			if (file_exists($target_file)):
+				echo "Sorry, file already exists.";
 				$uploadOk = 0;
 			endif;
-		endif;
 
-		// Check if file already exists
-		if (file_exists($target_file)):
-			echo "Sorry, file already exists.";
-			$uploadOk = 0;
-		endif;
-
-    	// Check file size
-		if ($_FILES["photo"]["size"] > 500000):
-			echo "Sorry, your file is too large.";
-			$uploadOk = 0;
-		endif;
-
-		// Allow certain file formats
-		if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" ):
-				echo "Sorry, only JPG, JPEG, PNG files are allowed.";
-			$uploadOk = 0;
-		endif;
-
-		// Check if $uploadOk is set to 0 by an error
-		if ($uploadOk == 0):
-			echo "Sorry, your file was not uploaded.";
-		// if everything is ok, try to upload file
-		else:
-			if (move_uploaded_file($_FILES["photo"]["tmp_name"], $target_file)):
-				echo "The file ". basename( $_FILES["photo"]["name"]). " has been uploaded.";
-			else:
-				echo "Sorry, there was an error uploading your file.";
+	    	// Check file size
+			if ($_FILES["photo"]["size"] > 500000):
+				echo "Sorry, your file is too large.";
+				$uploadOk = 0;
 			endif;
+
+			// Allow certain file formats
+			if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" ):
+				echo "Sorry, only JPG, JPEG, PNG files are allowed.";
+				$uploadOk = 0;
+			endif;
+
+			// Check if $uploadOk is set to 0 by an error
+			if ($uploadOk == 0):
+				echo "Sorry, your file was not uploaded.";
+			// if everything is ok, try to upload file
+			else:
+				if (move_uploaded_file($_FILES["photo"]["tmp_name"], $target_file)):
+					$post_data = array(
+						'user_id' => auth()->user()->id,
+						'legenda' => $request->legenda,
+						'img' => $target_file,
+						'localizacao' => '',
+						'pessoas_marcadas' => '',
+						'created_at' => date('Y-m-d H:i:s'),
+						'updated_at' => date('Y-m-d H:i:s')
+					);
+
+					$post = DB::table('posts')->insert($post_data);
+
+					$responseData = array('success'=>'1', 'message'=>"The file ". $_FILES["photo"]["size"]. " has been uploaded.");
+				else:
+					$responseData = array('success'=>'0', 'message'=>"Sorry, there was an error uploading your file.");
+				endif;
+			endif;
+		else:
+			$responseData = array('success'=>'0', 'message'=>'Sem imagem');
 		endif;
+		
 
-		$error = $_FILES["photo"];
-		$responseData = array('success'=>'1', 'message'=>$error);
-	else:
-		$error = $_FILES["photo"];
-		$responseData = array('success'=>'0', 'message'=>$error);
-	endif;
-
-	return response()->json(compact('responseData'));
-}
+		return response()->json(compact('responseData'));
+	}
 }
--- routes/api.php
+++ routes/api.php
@@ -38,4 +38,4 @@ use Illuminate\Support\Facades\Route;
 
 	Route::post('/like/{post_id}',							'Api\LikeController@like');
 
-	Route::post('/publicar/upload-img',						'Api\PostController@upload_img');
+	Route::post('/publicar/{legenda?}',						'Api\PostController@publicar');
