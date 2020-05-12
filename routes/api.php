83f2a61654628fcfb92077643eacdf6dab4e49f6
Guilherme Rosa <guilherme@live.com>
2020-05-12 13:46:59 -0300

    middleware inserido nas rotas para protege-las

--- routes/api.php
+++ routes/api.php
@@ -18,26 +18,28 @@ use Illuminate\Support\Facades\Route;
 
 	Route::post('/cadastrar', 								'Api\AuthController@cadastrar');
 
-	Route::post('/logout', 									'Api\AuthController@logout');
+	Route::group(['middleware' => ['apiJwt']], function(){
+		Route::post('/logout', 									'Api\AuthController@logout');
 
-	Route::get('/meu_perfil/',								'Api\PerfilController@meu_perfil');
+		Route::get('/meu_perfil/',								'Api\PerfilController@meu_perfil');
 
-	Route::get('/feed/',									'Api\FeedController@feed');
+		Route::get('/feed/',									'Api\FeedController@feed');
 
-	Route::get('/buscar/{texto?}',							'Api\BuscarController@buscar');
+		Route::get('/buscar/{texto?}',							'Api\BuscarController@buscar');
 
-	Route::get('/ver_perfil/{userid}',						'Api\PerfilController@ver_perfil');
+		Route::get('/ver_perfil/{userid}',						'Api\PerfilController@ver_perfil');
 
-	Route::post('/seguir/{userid}',							'Api\SeguirController@seguir');
+		Route::post('/seguir/{userid}',							'Api\SeguirController@seguir');
 
-	Route::post('/desseguir/{userid}',						'Api\SeguirController@desseguir');
+		Route::post('/desseguir/{userid}',						'Api\SeguirController@desseguir');
 
-	Route::get('/comentarios/{post_id}',					'Api\ComentariosController@comentarios');
+		Route::get('/comentarios/{post_id}',					'Api\ComentariosController@comentarios');
 
-	Route::post('/comentar/{post_id}/{texto}',				'Api\ComentariosController@comentar');
+		Route::post('/comentar/{post_id}/{texto}',				'Api\ComentariosController@comentar');
 
-	Route::post('/like/{post_id}',							'Api\LikeController@like');
+		Route::post('/like/{post_id}',							'Api\LikeController@like');
 
-	Route::post('/publicar/{legenda?}',						'Api\PostController@publicar');
+		Route::post('/publicar/{legenda?}',						'Api\PostController@publicar');
 
-	Route::post('/publicar/delete/{post_id}',				'Api\PostController@delete');
+		Route::post('/publicar/delete/{post_id}',				'Api\PostController@delete');
+	});
